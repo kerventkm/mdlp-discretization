66862ec4a8aca8aed83a37db0ab057666b626fd0
Kerven Durdymyradov <durdymk@kw-19509.kaust.edu.sa>
2025-03-12 11:57:49 +0300

    Reorganize setup files to prioritize GPU implementation

--- setup.py
+++ setup.py
@@ -1,65 +1,27 @@
-#!/usr/bin/env python
-"""
-Implements the MDLP discretization criterion from Usama Fayyad's paper
-"Multi-Interval Discretization of Continuous-Valued Attributes for
-Classification Learning."
-"""
+from setuptools import setup, find_packages
 
-from setuptools import Extension, find_packages, setup
+# Define packages to include, explicitly excluding the CPU implementation
+packages = find_packages(exclude=['mdlp._mdlp', 'mdlp.discretization'])
 
-if __name__ == '__main__':
-    # see https://stackoverflow.com/a/42163080 for the approach to pushing
-    # numpy and cython dependencies into extension building only
-    try:
-        # if Cython is available, we will rebuild from the pyx file directly
-        from Cython.Distutils import build_ext
-        print("Using cython to build the extension")
-        sources = ['mdlp/_mdlp.pyx']
-    except:
-        # else we build from the cpp file included in the distribution
-        print("Cannot find available cython installation. Using cpp to build the extension")
-        from setuptools.command.build_ext import build_ext
-        sources = ['mdlp/_mdlp.cpp']
-
-    class CustomBuildExt(build_ext):
-        """Custom build_ext class to defer numpy imports until needed.
-
-        Overrides the run command for building an extension and adds in numpy
-        include dirs to the extension build. Doing this at extension build time
-        allows us to avoid requiring that numpy be pre-installed before
-        executing this setup script.
-        """
-
-        def run(self):
-            import numpy
-            self.include_dirs.append(numpy.get_include())
-            build_ext.run(self)
-
-    cpp_ext = Extension(
-        'mdlp._mdlp',
-        sources=sources,
-        libraries=[],
-        include_dirs=[],
-        language='c++',
-    )
-
-    setup(
-        name='mdlp-discretization',
-        version='0.3.3',
-        description=__doc__,
-        license='BSD 3 Clause',
-        url='github.com/hlin117/mdlp-discretization',
-        author='Henry Lin',
-        author_email='hlin117@gmail.com',
-        install_requires=[
-            'numpy>=1.11.2',
-            'scipy>=0.18.1',
-            'scikit-learn>=0.18.1',
-        ],
-        setup_requires=[
-            'numpy>=1.11.2',
-        ],
-        packages=['mdlp'],
-        ext_modules=[cpp_ext],
-        cmdclass={'build_ext': CustomBuildExt},
-    )
+setup(
+    name='mdlp-discretization',
+    version='0.4.0',
+    description='GPU-accelerated MDLP discretization',
+    license='BSD 3 Clause',
+    author='Henry Lin',
+    author_email='hlin117@gmail.com',
+    packages=packages,
+    package_data={
+        'mdlp': ['*.py'],
+    },
+    exclude_package_data={
+        'mdlp': ['_mdlp.cpp', '_mdlp.pyx'],
+    },
+    install_requires=[
+        'numpy>=1.11.2',
+        'scipy>=0.18.1',
+        'scikit-learn>=0.18.1',
+        'cupy-cuda12x>=12.0.0',  # For CUDA 12.x
+    ],
+    python_requires='>=3.8',
+) 
